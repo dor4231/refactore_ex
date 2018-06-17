e4e5a52771bbb10f2d19a2856a50de39c8c79417
Dor Bar <dor4231@gmail.com>
2018-06-17 21:44:39 +0300

    refactor: Generate the table using js (Octopus)

--- js/app.js
+++ js/app.js
@@ -8,6 +8,55 @@ const model = {
     ]
 };
 
+const octopus = {
+    init: function() {
+        this.table = document.querySelector('.main-table');
+
+        this.createTable(12);
+    },
+
+    createTable: function (num) {
+    //    Create Headlines
+        const headlines = document.createElement('thead');
+        const headlineRow = document.createElement('tr');
+        this.createTableElement('th', 'Student Name', headlineRow, 'name-col');
+        for( let i = 1; i <= num; i++) {
+            this.createTableElement('th', i, headlineRow);
+        }
+        this.createTableElement('th', 'Days Missed-col', headlineRow, 'missed-col');
+
+        headlines.appendChild(headlineRow);
+    //    Loop names
+        const tableBody = document.createElement('tbody');
+        for (const name of model.attendanceNames) {
+            const attendaceRow = document.createElement('tr');
+            this.createTableElement('td', name, attendaceRow, 'name-col');
+            for( let i = 1; i <= num; i++) {
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                this.createTableElement('td', checkbox, attendaceRow, 'attend-col');
+            }
+            this.createTableElement('td', 0, attendaceRow, 'missed-col');
+            tableBody.appendChild(attendaceRow);
+        }
+
+
+        this.table.appendChild(headlines);
+        this.table.appendChild(tableBody);
+    },
+
+    createTableElement: function (type, innerText, appendTo, setClass = null) {
+        const element = document.createElement(type);
+        if(setClass) element.classList.add(setClass);
+        (typeof(innerText) === 'object') ? element.appendChild(innerText) : element.innerHTML = innerText;
+        appendTo.appendChild(element);
+    }
+};
+
+(function() {
+    octopus.init();
+})();
+
 /* STUDENTS IGNORE THIS FUNCTION
  * All this does is create an initial
  * attendance record if one is not found
